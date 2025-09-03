9e37d1a79bcd8a495913109da838315e63af1516
Thusitha Sampath <thusithas120@gmail.com>
2025-09-03 08:38:48 +0530

    installed react icons

--- src/app/components/Services.js
+++ src/app/components/Services.js
@@ -1,4 +1,5 @@
 import { Code2, Palette, Server, Smartphone } from "lucide-react";
+import { FaWordpress } from "react-icons/fa";
 import React from "react";
 
 function Services() {
@@ -7,31 +8,31 @@ function Services() {
       name: "Frontend Development",
       description:
         "Building responsive, user friendly, and dynamic interfaces using React, Next.js, and modern frameworks for a seamless user experience.",
-      icon: <Code2 />,
+      icon: <Code2 size={32} />,
     },
     {
       name: "Backend Development",
       description:
         "Developing secure, scalable, and efficient server side solutions with Laravel, CodeIgniter, and MySQL to power your applications.",
-      icon: <Server />,
+      icon: <Server size={32} />,
     },
     {
       name: "Mobile Application Development",
       description:
         "Creating cross platform mobile apps with React Native and Android Studio (Java), ensuring smooth performance and intuitive design.",
-      icon: <Smartphone />,
+      icon: <Smartphone size={32} />,
     },
     {
       name: "Wordpress Development",
       description:
         "Developing and customizing WordPress websites that are fast, SEO friendly, and tailored to meet business needs.",
-      icon: "",
+      icon: <FaWordpress size={32} />,
     },
     {
       name: "UI/UX Design",
       description:
         "Crafting modern, engaging, and intuitive user experiences with Figma, focusing on usability and aesthetics.",
-      icon: <Palette />,
+      icon: <Palette size={32} />,
     },
   ];
   return (
@@ -56,11 +57,11 @@ function Services() {
               key={index}
               className="relative bg-gray-800/50 backdrop-blur-lg shadow-xl border border-[var(--primary-color)]/20 flex flex-col  gap-2 p-3 rounded-2xl overflow-hidden group hover:transform hover:-translate-y-2 transition-all duration-300 "
             >
-              <div className="w-15 h-15 rounded-2xl  border-2 border-[var(--primary-color)] flex items-center justify-center mb-4 group-hover:scale-105 transition-transform duration-300">
+              <div className="w-12 h-12 md:w-15 md:h-15 rounded-2xl border-2 border-[var(--primary-color)] flex items-center justify-center mb-4 group-hover:scale-105 transition-transform duration-300">
                 {service.icon}
               </div>
 
-              <h3 className="text-xl md:text-2xl font-medium group-hover:text-[#66e5ff] transition-colors duration-300">
+              <h3 className="text-lg md:text-2xl font-medium group-hover:text-[#66e5ff] transition-colors duration-300">
                 {service.name}
               </h3>
               <p className="text-sm md:text-[16px] text-[var(--text-color)] leading-relaxed">
